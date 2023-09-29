854a47434bb3491debe8101bdfa8068fd1d8a178
Jinah-Ma <1618wlsdk@gmail.com>
2023-09-29 16:08:21 +0900

    햄버거 메뉴 구현 시작

--- js/basic.js
+++ js/basic.js
@@ -1,3 +1,10 @@
+/* 언어설정 */
+$('.language button').on('click', function(){
+    $('ul.Language li a').toggle();
+
+})
+
+/* 팝업구현 */
 $(function () {
     $('#wrap').fullpage({
         anchors: ["section1", "section2", "section3", "section4", "section5", "section6"],
@@ -14,7 +21,6 @@ $(() => {
         $('.popup').hide();
     });
 });
-// popup
 
 /* 서브배너/ 전시 */
 let slideBtn1 = $('.sabBanner .oneCard');
@@ -130,3 +136,42 @@ slideBtn32.on('click', function () {
 
 /* 메인뉴스 */
 
+let newBtnObj = $('.titleH3 h3');
+let newsObj = $('.mainNews .title .hide');
+
+/* 각 버튼과 탭 내용 */
+newBtnObj.on('click', function () {
+    let index = newBtnObj.index(this);
+    $(this).addClass('on');
+    newsObj.eq(index).addClass('on');
+    $(this).css('color', '#fff');
+    $(this).css('background-color', '#222');
+    newsObj.eq(index).css('display', 'block');
+
+
+    newBtnObj.not(this).removeClass('on');
+    newsObj.not(newsObj.eq(index)).removeClass('on');
+    newBtnObj.not(this).css('color', '#222');
+    newBtnObj.not(this).css('background-color', '#fff');
+    newsObj.not(newsObj.eq(index)).css('display', 'none');
+});
+
+
+/* 팝업 슬라이드 구현 */
+
+
+
+/* 푸터 사이트 버튼 구현 */
+let siteButton1 = $('.footerMore .site1 button');
+let siteButton2 = $('.footerMore .site2 button');
+let siteButton3 = $('.footerMore .site3 button');
+
+siteButton1.on('click', function(){
+    $('.flexRow .site1 ul').toggle();
+})
+siteButton2.on('click', function(){
+    $('.flexRow .site2 ul').toggle();
+})
+siteButton3.on('click', function(){
+    $('.flexRow .site3 ul').toggle();
+})
--- js/swiper.js
+++ js/swiper.js
@@ -1,37 +1,37 @@
 /* 스와이퍼 슬라이드 1 */
 let swiper = new Swiper(".mySwiper", {
-    /* spaceBetween: 30, */
-    centeredSlides: true,
-    autoplay: {
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
-      }
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  /* spaceBetween: 30, */
+  centeredSlides: true,
+  autoplay: {
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    }
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /* 재생 정지 버튼 */
-  $('.swiper-button-pause').on('click', function(){
-    swiper.autoplay.stop();
-    $(this).removeClass('active');
-    $('.swiper-button-play').addClass('active');
-    $('.swiper-button-pause').css('display', 'none');
-    $('.swiper-button-play').css('display', 'block');
-    return false;
+$('.swiper-button-pause').on('click', function () {
+  swiper.autoplay.stop();
+  $(this).removeClass('active');
+  $('.swiper-button-play').addClass('active');
+  $('.swiper-button-pause').css('display', 'none');
+  $('.swiper-button-play').css('display', 'block');
+  return false;
 });
-$('.swiper-button-play').on('click', function(){
-    swiper.autoplay.start();
-    $(this).removeClass('active');
-    $('.swiper-button-pause').addClass('active');
-    $('.swiper-button-play').css('display', 'none');
-    $('.swiper-button-pause').css('display', 'block');
-    return false;
+$('.swiper-button-play').on('click', function () {
+  swiper.autoplay.start();
+  $(this).removeClass('active');
+  $('.swiper-button-pause').addClass('active');
+  $('.swiper-button-play').css('display', 'none');
+  $('.swiper-button-pause').css('display', 'block');
+  return false;
 });
